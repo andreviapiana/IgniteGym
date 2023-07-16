7f7965ea1bc43ee482f79c99302435a6e2fa3651
andreviapiana <andreviapiana@gmail.com>
2023-07-16 20:31:11 -0300

    ✨feat: Home - Home - Componente Group - Exibindo o Grupo Selecionado

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,14 +1,25 @@
 import { Group } from '@components/Group'
 import { HomeHeader } from '@components/HomeHeader'
 import { HStack, VStack } from 'native-base'
+import { useState } from 'react'
 
 export function Home() {
+  const [groupSelected, setGroupSelected] = useState('Costa')
+
   return (
     <VStack flex={1}>
       <HomeHeader />
       <HStack>
-        <Group name="Costa" />
-        <Group name="Ombro" />
+        <Group
+          name="costa"
+          isActive={groupSelected === 'costa'}
+          onPress={() => setGroupSelected('costa')}
+        />
+        <Group
+          name="ombro"
+          isActive={groupSelected === 'ombro'}
+          onPress={() => setGroupSelected('ombro')}
+        />
       </HStack>
     </VStack>
   )

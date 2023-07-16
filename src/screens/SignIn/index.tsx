5c48ce148637d5ec7154084cd3ba509cacc6eb9c
andreviapiana <andreviapiana@gmail.com>
2023-07-16 19:12:25 -0300

    ✨feat: Rotas Públicas - Navegação de Autenticação

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -6,7 +6,16 @@ import LogoSvg from '@assets/logo.svg'
 import { Input } from '@components/Input'
 import { Button } from '@components/Button'
 
+import { useNavigation } from '@react-navigation/native'
+import { AuthNavigatorRoutesProps } from '@routes/auth.routes'
+
 export function SignIn() {
+  const navigation = useNavigation<AuthNavigatorRoutesProps>()
+
+  function handleNewAccount() {
+    navigation.navigate('signUp')
+  }
+
   return (
     <ScrollView
       contentContainerStyle={{ flexGrow: 1 }}
@@ -49,7 +58,11 @@ export function SignIn() {
           </Text>
         </Center>
 
-        <Button title="Criar Conta" variant="outline" />
+        <Button
+          title="Criar Conta"
+          variant="outline"
+          onPress={handleNewAccount}
+        />
       </VStack>
     </ScrollView>
   )
--- src/screens/SignUp/index.tsx
+++ src/screens/SignUp/index.tsx
@@ -6,7 +6,15 @@ import LogoSvg from '@assets/logo.svg'
 import { Input } from '@components/Input'
 import { Button } from '@components/Button'
 
+import { useNavigation } from '@react-navigation/native'
+
 export function SignUp() {
+  const navigation = useNavigation()
+
+  function handleGoBack() {
+    navigation.goBack()
+  }
+
   return (
     <ScrollView
       contentContainerStyle={{ flexGrow: 1 }}
@@ -45,7 +53,12 @@ export function SignUp() {
           <Button title="Criar e acessar" />
         </Center>
 
-        <Button title="Voltar para o login" variant="outline" mt={24} />
+        <Button
+          title="Voltar para o login"
+          variant="outline"
+          mt={24}
+          onPress={handleGoBack}
+        />
       </VStack>
     </ScrollView>
   )

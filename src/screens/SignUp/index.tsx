1fb342ddf8001b668800301e52b6b5bd30a83450
andreviapiana <andreviapiana@gmail.com>
2023-07-16 19:02:08 -0300

    ✨feat: Rotas Públicas - Aplicando o Tema na Navegação

--- src/screens/SignUp/index.tsx
+++ src/screens/SignUp/index.tsx
@@ -12,7 +12,7 @@ export function SignUp() {
       contentContainerStyle={{ flexGrow: 1 }}
       showsVerticalScrollIndicator={false}
     >
-      <VStack flex={1} bg="gray.700" px={10} pb={16}>
+      <VStack flex={1} px={10} pb={16}>
         <Image
           source={BackgroundImg}
           alt="Pessoas treinando"

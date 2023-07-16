0a16fc158f240165425ea1f0b8987d966a0575eb
andreviapiana <andreviapiana@gmail.com>
2023-07-16 17:54:54 -0300

    ✨feat: SignIn - Componente Input - Focus + Finalização

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -8,7 +8,7 @@ import { Input } from '@components/Input'
 
 export function SignIn() {
   return (
-    <VStack flex={1} bg="gray.700">
+    <VStack flex={1} bg="gray.700" px={10}>
       <Image
         source={BackgroundImg}
         alt="Pessoas treinando"
@@ -28,8 +28,12 @@ export function SignIn() {
           Acesse a conta
         </Heading>
 
-        <Input placeholder="E-mail" />
-        <Input placeholder="Senha" />
+        <Input
+          placeholder="E-mail"
+          keyboardType="email-address"
+          autoCapitalize="none"
+        />
+        <Input placeholder="Senha" secureTextEntry />
       </Center>
     </VStack>
   )

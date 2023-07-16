8e586b23dc68045b5fcdef7de76bad3593e55b53
andreviapiana <andreviapiana@gmail.com>
2023-07-16 19:53:08 -0300

    ✨feat: Rotas Privadas - Estilizando a Bottom Navigator

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -3,6 +3,7 @@ import {
   createBottomTabNavigator,
 } from '@react-navigation/bottom-tabs'
 import { useTheme } from 'native-base'
+import { Platform } from 'react-native'
 
 import HomeSvg from '@assets/home.svg'
 import HistorySvg from '@assets/history.svg'
@@ -36,6 +37,13 @@ export function AppRoutes() {
         tabBarShowLabel: false,
         tabBarActiveTintColor: colors.green[500],
         tabBarInactiveTintColor: colors.gray[200],
+        tabBarStyle: {
+          backgroundColor: colors.gray[600],
+          borderTopWidth: 0,
+          height: Platform.OS === 'android' ? 'auto' : 96,
+          paddingBottom: sizes[10],
+          paddingTop: sizes[6],
+        },
       }}
     >
       <Screen
@@ -68,7 +76,11 @@ export function AppRoutes() {
         }}
       />
 
-      <Screen name="exercise" component={Exercise} />
+      <Screen
+        name="exercise"
+        component={Exercise}
+        options={{ tabBarButton: () => null }}
+      />
     </Navigator>
   )
 }

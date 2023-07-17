3e1b05da46dabd1aea23cf9a5d3b428b6f8da51a
andreviapiana <andreviapiana@gmail.com>
2023-07-17 12:32:17 -0300

    ✨feat: Exercises - Finalizando o Header dessa tela

--- src/screens/Exercise/index.tsx
+++ src/screens/Exercise/index.tsx
@@ -1,8 +1,9 @@
-import { Icon, VStack } from 'native-base'
+import { HStack, Heading, Icon, VStack, Text } from 'native-base'
 import { useNavigation } from '@react-navigation/native'
 import { AppNavigatorRoutesProps } from '@routes/app.routes'
 import { TouchableOpacity } from 'react-native'
 import { Feather } from '@expo/vector-icons'
+import BodySvg from '@assets/body.svg'
 
 export function Exercise() {
   const navigation = useNavigation<AppNavigatorRoutesProps>()
@@ -17,6 +18,25 @@ export function Exercise() {
         <TouchableOpacity onPress={handleGoBack}>
           <Icon as={Feather} name="arrow-left" color="green.500" size={6} />
         </TouchableOpacity>
+
+        <HStack
+          justifyContent="space-between"
+          mt={4}
+          mb={8}
+          alignItems="center"
+        >
+          <Heading color="gray.100" fontSize="lg" flexShrink={1}>
+            Puxada frontal
+          </Heading>
+
+          <HStack alignItems="center">
+            <BodySvg />
+
+            <Text color="gray.200" ml={1} textTransform="capitalize">
+              Costas
+            </Text>
+          </HStack>
+        </HStack>
       </VStack>
     </VStack>
   )

99510d7f1db67565b29529e7138765d837befaf7
andreviapiana <andreviapiana@gmail.com>
2023-07-17 20:46:25 -0300

    ✨feat: Galeria de Fotos - Ativando o carregamento da foto

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -25,18 +25,28 @@ export function Profile() {
   )
 
   async function handleUserPhotoSelected() {
-    const photoSelected = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Images,
-      quality: 1,
-      aspect: [4, 4],
-      allowsEditing: true,
-    })
+    setPhotoIsLoading(true)
 
-    if (photoSelected.canceled) {
-      return
+    try {
+      const photoSelected = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+        aspect: [4, 4],
+        allowsEditing: true,
+      })
+
+      if (photoSelected.canceled) {
+        return
+      }
+
+      if (photoSelected.assets[0].uri) {
+        setUserPhoto(photoSelected.assets[0].uri)
+      }
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setPhotoIsLoading(false)
     }
-
-    setUserPhoto(photoSelected.assets[0].uri)
   }
 
   return (

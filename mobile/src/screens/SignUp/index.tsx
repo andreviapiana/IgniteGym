9e1210ee72226a87d461b4b0f0be6eefdd2249ce
andreviapiana <andreviapiana@gmail.com>
2023-07-18 20:25:06 -0300

    ✨feat: API - Obtendo Resposta do Back-End

--- mobile/src/screens/SignUp/index.tsx
+++ mobile/src/screens/SignUp/index.tsx
@@ -58,6 +58,8 @@ export function SignUp() {
       },
       body: JSON.stringify({ name, email, password }),
     })
+      .then((response) => response.json())
+      .then((data) => console.log(data))
   }
 
   return (

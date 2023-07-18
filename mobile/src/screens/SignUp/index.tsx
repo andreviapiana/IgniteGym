9ce097b4c359c0e7ac57e8ebd4d872a2a77bdc53
andreviapiana <andreviapiana@gmail.com>
2023-07-18 20:29:45 -0300

    ✨feat: API - Async Await

--- mobile/src/screens/SignUp/index.tsx
+++ mobile/src/screens/SignUp/index.tsx
@@ -49,8 +49,8 @@ export function SignUp() {
   }
 
   // Função de SignUp //
-  function handleSignUp({ name, email, password }: FormDataProps) {
-    fetch('http://192.168.0.101:3333/users', {
+  async function handleSignUp({ name, email, password }: FormDataProps) {
+    const response = await fetch('http://192.168.0.101:3333/users', {
       method: 'POST',
       headers: {
         Accept: 'application/json',
@@ -58,8 +58,8 @@ export function SignUp() {
       },
       body: JSON.stringify({ name, email, password }),
     })
-      .then((response) => response.json())
-      .then((data) => console.log(data))
+    const data = await response.json()
+    console.log(data)
   }
 
   return (

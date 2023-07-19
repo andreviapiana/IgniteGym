6dd6529160faafbdc77b70a79f4806127a5e96e7
andreviapiana <andreviapiana@gmail.com>
2023-07-19 10:39:33 -0300

    ✨feat: Contextos - Tipando o Contexto

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -25,10 +25,12 @@ export default function App() {
       />
       <AuthContext.Provider
         value={{
-          id: '1',
-          name: 'Rodrigo Gonçalves',
-          email: 'rodrigo@email.com',
-          avatar: 'rodrigo.png',
+          user: {
+            id: '1',
+            name: 'Rodrigo Gonçalves',
+            email: 'rodrigo@email.com',
+            avatar: 'rodrigo.png',
+          },
         }}
       >
         {fontsLoaded ? <Routes /> : <Loading />}

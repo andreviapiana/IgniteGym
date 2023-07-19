d966fa0c2543721b15fb494dc6381b83fac98d13
andreviapiana <andreviapiana@gmail.com>
2023-07-18 21:18:12 -0300

    ✨feat: HTTP Client - Utilizando o Axios

--- mobile/src/screens/SignUp/index.tsx
+++ mobile/src/screens/SignUp/index.tsx
@@ -11,6 +11,8 @@ import { Controller, useForm } from 'react-hook-form'
 import * as yup from 'yup'
 import { yupResolver } from '@hookform/resolvers/yup'
 
+import { api } from '@services/api'
+
 type FormDataProps = {
   name: string
   email: string
@@ -50,16 +52,8 @@ export function SignUp() {
 
   // Função de SignUp //
   async function handleSignUp({ name, email, password }: FormDataProps) {
-    const response = await fetch('http://192.168.0.101:3333/users', {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ name, email, password }),
-    })
-    const data = await response.json()
-    console.log(data)
+    const response = await api.post('/users', { name, email, password })
+    console.log(response.data)
   }
 
   return (

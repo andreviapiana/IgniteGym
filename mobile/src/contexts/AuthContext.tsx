b352144edb257fb31f0e119b6b35eac92e5fa383
andreviapiana <andreviapiana@gmail.com>
2023-07-19 14:18:00 -0300

    ✨feat: Contextos - Compartilhando Estados no Contexto

--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, createContext } from 'react'
+import { ReactNode, createContext, useState } from 'react'
 
 import { UserDTO } from '@dtos/UserDTO'
 
@@ -15,18 +15,14 @@ export const AuthContext = createContext<AuthContextDataProps>(
 )
 
 export function AuthContextProvider({ children }: AuthContextProviderProps) {
+  const [user, setUser] = useState({
+    id: '1',
+    name: 'Rodrigo',
+    email: 'rodrigo@email.com',
+    avatar: 'rodrigo.png',
+  })
+
   return (
-    <AuthContext.Provider
-      value={{
-        user: {
-          id: '1',
-          name: 'Rodrigo Gonçalves',
-          email: 'rodrigo@email.com',
-          avatar: 'rodrigo.png',
-        },
-      }}
-    >
-      {children}
-    </AuthContext.Provider>
+    <AuthContext.Provider value={{ user }}>{children}</AuthContext.Provider>
   )
 }

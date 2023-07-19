be4100d65b15c8a8c18e8372d9fb01c72a9fac74
andreviapiana <andreviapiana@gmail.com>
2023-07-18 21:20:57 -0300

    ✨feat: HTTP Client - Axios Exceptions

--- mobile/src/screens/SignUp/index.tsx
+++ mobile/src/screens/SignUp/index.tsx
@@ -12,6 +12,8 @@ import * as yup from 'yup'
 import { yupResolver } from '@hookform/resolvers/yup'
 
 import { api } from '@services/api'
+import axios from 'axios'
+import { Alert } from 'react-native'
 
 type FormDataProps = {
   name: string
@@ -52,8 +54,14 @@ export function SignUp() {
 
   // Função de SignUp //
   async function handleSignUp({ name, email, password }: FormDataProps) {
-    const response = await api.post('/users', { name, email, password })
-    console.log(response.data)
+    try {
+      const response = await api.post('/users', { name, email, password })
+      console.log(response.data)
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        Alert.alert(error.response?.data.message)
+      }
+    }
   }
 
   return (

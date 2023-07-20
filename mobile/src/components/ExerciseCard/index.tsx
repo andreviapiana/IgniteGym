4a76a8938bea68657debfd5d196946cf0cb48a4f
andreviapiana <andreviapiana@gmail.com>
2023-07-20 14:26:29 -0300

    ✨feat: Consumindo o BackEnd – Carregando Imagem dos Exercícios

--- mobile/src/components/ExerciseCard/index.tsx
+++ mobile/src/components/ExerciseCard/index.tsx
@@ -4,6 +4,7 @@ import { Heading, HStack, Image, Text, VStack, Icon } from 'native-base'
 import { Entypo } from '@expo/vector-icons'
 
 import { ExerciseDTO } from '@dtos/ExerciseDTO'
+import { api } from '@services/api'
 
 type Props = TouchableOpacityProps & {
   data: ExerciseDTO
@@ -22,7 +23,7 @@ export function ExerciseCard({ data, ...rest }: Props) {
       >
         <Image
           source={{
-            uri: 'http://conteudo.imguol.com.br/c/entretenimento/0c/2019/12/03/remada-unilateral-com-halteres-1575402100538_v2_600x600.jpg',
+            uri: `${api.defaults.baseURL}/exercise/thumb/${data.thumb}`,
           }}
           alt="Imagem do exercício"
           w={16}

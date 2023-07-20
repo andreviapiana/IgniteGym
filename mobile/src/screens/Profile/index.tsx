4d045e2598abae3d99af6463b25d4a1e84ffc502
andreviapiana <andreviapiana@gmail.com>
2023-07-20 20:50:47 -0300

    ✨feat: Consumindo o BackEnd -  Validação Condicional da Senha - Yup

--- mobile/src/screens/Profile/index.tsx
+++ mobile/src/screens/Profile/index.tsx
@@ -46,7 +46,11 @@ const profileSchema = yup.object({
     .string()
     .nullable()
     .transform((value) => value || null)
-    .oneOf([yup.ref('password'), null], 'A confirmação de senha não confere.'),
+    .oneOf([yup.ref('password'), null], 'A confirmação de senha não confere.')
+    .when('password', {
+      is: (Field: any) => Field,
+      then: yup.string().nullable().required('Informe a confirmação da senha.'),
+    }),
 })
 
 export function Profile() {

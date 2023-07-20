09886907beddf97254f82e4a58b149a38cd2fa7a
andreviapiana <andreviapiana@gmail.com>
2023-07-20 10:23:58 -0300

    ✨feat: Autenticação JWT - Anexando o Token nas Requisições

--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -7,6 +7,7 @@ import {
   storageUserRemove,
   storageUserSave,
 } from '@storage/storageUser'
+import { storageAuthTokenSave } from '@storage/storageAuthToken'
 
 export type AuthContextDataProps = {
   user: UserDTO
@@ -27,13 +28,28 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
   const [user, setUser] = useState<UserDTO>({} as UserDTO)
   const [isLoadingUserStorageData, setIsLoadingUserStorageData] = useState(true)
 
+  async function storageUserAndToken(userData: UserDTO, token: string) {
+    try {
+      setIsLoadingUserStorageData(true)
+
+      api.defaults.headers.common.Authorization = `Bearer ${token}`
+
+      await storageUserSave(userData)
+      await storageAuthTokenSave(token)
+      setUser(userData)
+    } catch (error) {
+      throw error
+    } finally {
+      setIsLoadingUserStorageData(false)
+    }
+  }
+
   async function signIn(email: string, password: string) {
     try {
       const { data } = await api.post('/sessions', { email, password })
 
-      if(data.user && data.token) {
-        setUser(data.user)
-        storageUserSave(data.user)
+      if (data.user && data.token) {
+        storageUserAndToken(data.user, data.token)
       }
     } catch (error) {
       throw error

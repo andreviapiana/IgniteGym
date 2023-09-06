9ea127cbb6eaebe31736fbad4b251aaf55c002c2
andreviapiana <andreviapiana@gmail.com>
2023-09-06 15:20:36 -0300

    ✨feat: Modulo 04 - Push Notifications – Componente de Notificação

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -2,7 +2,7 @@ import { StatusBar } from 'react-native'
 import { NativeBaseProvider } from 'native-base'
 import { THEME } from './src/theme'
 
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 import {
   useFonts,
@@ -12,10 +12,14 @@ import {
 
 import { Routes } from '@routes/index'
 import { Loading } from '@components/Loading'
+import { Notification } from '@components/Notification'
 
 import { AuthContextProvider } from '@contexts/AuthContext'
 
-import OneSignal, { NotificationReceivedEvent } from 'react-native-onesignal'
+import OneSignal, {
+  NotificationReceivedEvent,
+  OSNotification,
+} from 'react-native-onesignal'
 import { REACT_APP_ONE_SIGNAL_APP_ID } from '@env'
 
 OneSignal.setAppId(String(REACT_APP_ONE_SIGNAL_APP_ID))
@@ -25,10 +29,14 @@ OneSignal.promptForPushNotificationsWithUserResponse()
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
 
+  const [notification, setNotification] = useState<OSNotification>()
+
   useEffect(() => {
     const unsubscribe = OneSignal.setNotificationWillShowInForegroundHandler(
       (notificationRecivedEvent: NotificationReceivedEvent) => {
-        console.log(notificationRecivedEvent)
+        const response = notificationRecivedEvent.getNotification()
+
+        setNotification(response)
       },
     )
 
@@ -45,6 +53,13 @@ export default function App() {
       <AuthContextProvider>
         {fontsLoaded ? <Routes /> : <Loading />}
       </AuthContextProvider>
+
+      {notification?.title && (
+        <Notification
+          title={notification.title}
+          onClose={() => setNotification(undefined)}
+        />
+      )}
     </NativeBaseProvider>
   )
 }

0516df3f9a4f0e506e2fa554eac6fa76ce20bb1c
andreviapiana <andreviapiana@gmail.com>
2023-09-06 15:13:01 -0300

    ✨feat: Modulo 04 - Push Notifications – Mensagem em Foreground

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -2,6 +2,8 @@ import { StatusBar } from 'react-native'
 import { NativeBaseProvider } from 'native-base'
 import { THEME } from './src/theme'
 
+import { useEffect } from 'react'
+
 import {
   useFonts,
   Roboto_400Regular,
@@ -13,13 +15,25 @@ import { Loading } from '@components/Loading'
 
 import { AuthContextProvider } from '@contexts/AuthContext'
 
-import OneSignal from 'react-native-onesignal'
+import OneSignal, { NotificationReceivedEvent } from 'react-native-onesignal'
 import { REACT_APP_ONE_SIGNAL_APP_ID } from '@env'
 
 OneSignal.setAppId(String(REACT_APP_ONE_SIGNAL_APP_ID))
 
+OneSignal.promptForPushNotificationsWithUserResponse()
+
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
+
+  useEffect(() => {
+    const unsubscribe = OneSignal.setNotificationWillShowInForegroundHandler(
+      (notificationRecivedEvent: NotificationReceivedEvent) => {
+        console.log(notificationRecivedEvent)
+      },
+    )
+
+    return () => unsubscribe
+  }, [])
 
   return (
     <NativeBaseProvider theme={THEME}>
